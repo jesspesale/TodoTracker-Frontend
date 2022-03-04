f93901f46262c26dd40f274ba3597309ef675bb9
jesspesale <jess_pesale@aol.com>
2022-03-03 22:41:49 -0500

    added imports for redux, store and thunk to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore, applyMiddleware} from 'redux'
+import {thunk} from 'redux-thunk'
+import {Provider} from 'react-redux'
+
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
