763edda2423588106ebb300f9bcd2ab440e0d888
jesspesale <jess_pesale@aol.com>
2022-03-03 22:08:19 -0500

    successfully fetched data from the rails api backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>React App</h2>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/lists")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h2>React App</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
