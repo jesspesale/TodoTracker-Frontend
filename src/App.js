c2bc6147853366329df0e336065b9b3970886696
jesspesale <jess_pesale@aol.com>
2022-03-03 22:39:06 -0500

    added all redux, thunk, router packages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch("http://localhost:3000/api/v1/lists")
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
+  // componentDidMount() {
+  //   fetch("http://localhost:3000/api/v1/lists/1")
+  //   .then(res => res.json())
+  //   .then(data => console.log(data))
+  // }
 
   render(){
     return (
