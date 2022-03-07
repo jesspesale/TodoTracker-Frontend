8e6690a0ce9361dc854341aeca17903cbbbf02b2
jesspesale <jess_pesale@aol.com>
2022-03-06 23:07:31 -0500

    added most things for redux and set up reducer

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import App from './App';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
+import {BrowserRouter as Router } from 'react-router-dom'
+import store from "./redux/store"
 
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// set up store, import provider to use redux
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+// // set up store, import provider to use redux
+// let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
+    <Router>
     <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
---
+++ src/redux/todoReducer.js
@@ -0,0 +1,10 @@
+export default function todoReducer(state = {todo: []}, action){
+    switch(action.type) {
+    //     case value: 
+    //     break;
+
+        default: 
+            return state
+    }
+
+}
