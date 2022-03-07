1b3448d8d2779fc37e85209d9e31555ac8f65cf3
jesspesale <jess_pesale@aol.com>
2022-03-06 23:34:27 -0500

    set up set todos in reducer

--- src/redux/todoReducer.js
+++ src/redux/todoReducer.js
@@ -3,6 +3,7 @@ import {SET_TODOS} from './constants'
 export default function todoReducer(state = {todo: []}, action){
     switch(action.type) {
         case "SET_TODOS": 
+        console.log(state)
             return action.payload
         break;
 
