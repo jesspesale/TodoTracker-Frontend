3fcfdeaab24fb0733456bb9980f012498c3eeb28
jesspesale <jess_pesale@aol.com>
2022-03-06 23:13:11 -0500

    added routes and home and notes link to navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import {Routes, Route, Link} from "react-router-dom"
+import TodoList from "./components/TodoList"
 
 
 class App extends React.Component {
@@ -14,6 +16,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <h2>React App</h2>
+        <Link to="/">Home</Link>
+        <Link to="/todos">Todos</Link>
+        <Routes>
+          <Route path="/todos" element={<TodoList/>}/>
+        </Routes>
       </div>
     );
   }
