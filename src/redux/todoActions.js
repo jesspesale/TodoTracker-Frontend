4a682ed064adba8266c2c083068224f0ae481138
jesspesale <jess_pesale@aol.com>
2022-03-06 23:34:47 -0500

    set up fetch todos actions

--- src/redux/todoActions.js
+++ src/redux/todoActions.js
@@ -1,3 +1,9 @@
 import {SET_TODOS} from './constants'
 
-
+export function fetchTodos(){
+    return (dispatch) => {
+        fetch("http://localhost:3000/api/v1/lists/1")
+        .then(res => res.json())
+        .then(todos => dispatch({type: SET_TODOS, payload: todos}))
+    }
+}
