e974b5caeaaeae913f8da27449d68ce47366790e
jesspesale <jess_pesale@aol.com>
2022-03-07 20:54:19 -0500

    fetch todos working correctly and passing them down as probs to individual todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import {useEffect} from 'react'
+import {useSelector, useDispatch} from "react-redux"
+import { fetchTodos } from '../redux/todoActions';
+import Todo from './Todo';
 
 export default function TodoList() {
+  const todos = useSelector(state => state);
+  // basically replaces mapStateToProps
+  const dispatch = useDispatch()
+  // basically replaces mapDispatchToProps
+
+  useEffect(() => {
+    dispatch(fetchTodos())
+  }, [])
+
+  console.log(todos)
+
   return (
-    <div>TodoList</div>
+  <div>
+    TodoList
+    {/* {renderTodos()} */}
+    {todos && todos.map(todo => {
+      return <Todo todo={todo} key={todo.id}/>
+    })}
+  </div>
   )
 }
